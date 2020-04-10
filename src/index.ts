d49d0164fb8c2c81dbbc4eed44bea56ec758d86d
Daniel G <danielgulic@gmail.com>
2020-04-11 05:49:48 +1000

    export some stuff in index (TBC)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { default } from './CharmClient';
+export { default as Command } from './command/CommandDecorator';
+export { default as Module } from './module/Module';
+export { default as CommandExecution } from './command/CommandExecution';
+export { default as Optional } from './command/OptionalDecorator';
