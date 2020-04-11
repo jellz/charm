0382e5a4edea97a6bd51fa7e4997800b321dca2d
Daniel G <danielgulic@gmail.com>
2020-04-12 01:59:56 +1000

    clean up lib exports + export EventHandler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+// Client
 export { default } from './CharmClient';
-export { default as Command } from './command/CommandDecorator';
+
+// Decorators
+export { default as Command } from './command/decorator/CommandDecorator';
+export { default as Optional } from './command/decorator/OptionalDecorator';
+export { default as EventHandler } from './eventHandler/EventHandlerDecorator';
+
 export { default as Module } from './module/Module';
 export { default as CommandExecution } from './command/CommandExecution';
-export { default as Optional } from './command/OptionalDecorator';
